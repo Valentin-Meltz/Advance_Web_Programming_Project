04c41eb399bbedfa7fd63ce30599cc52fe620a5f
Valentin Meltz <seth_valou@MacBook-Pro-de-Valentin-4.local>
2024-11-29 22:11:20 -0500

    update url

--- data/main.js
+++ data/main.js
@@ -2,21 +2,21 @@ const packages = [
     {
         title: "Basketball",
         desc: "Basketball is a fast-paced game played between two teams of five players on a rectangular court, where the objective is to shoot the ball through the opponent's hoop.",
-        image: "../assets/basketball-player.png"
+        image: "https://static.vecteezy.com/system/resources/previews/027/309/410/non_2x/basketball-player-with-ai-generated-free-png.png"
     },
     {
         title: "Hockey",
         desc: "Hockey is a team sport played on ice, in which skaters use sticks to direct a puck into the opposing team's goal.",
-        image: "../assets/hockey-player.png" // Replace with actual image URL
+        image: "https://file.aiquickdraw.com/imgcompressed/img/compressed_b048c4c50ebff8ec8819814eac1a2d18.webp" 
     },
     {
         title: "Baseball",
         desc: "Baseball is a bat-and-ball game played between two opposing teams that take turns batting and fielding. The aim is to score runs by hitting a ball and running around bases.",
-        image: "../assets/baseball-player.png" // Replace with actual image URL
+        image: "https://static.vecteezy.com/system/resources/previews/028/195/644/non_2x/baseball-player-with-ai-generated-free-png.png" 
     },
     {
         title: "Curling",
         desc: "Curling is a sport in which players slide stones on a sheet of ice towards a target area, with the aim of accumulating the highest score.",
-        image: "../assets/curling.png" // Replace with actual image URL
+        image: "https://cdn3d.iconscout.com/3d/premium/thumb/curling-12006341-9792133.png"
     }
 ];

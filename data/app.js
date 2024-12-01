9b695c57c0b37ffa093303b6fc04b54976285caf
Valentin Meltz <seth_valou@MacBook-Pro-de-Valentin-4.local>
2024-12-01 10:30:59 -0500

    Update file

--- data/app.js
+++ data/app.js
@@ -1,6 +1,22 @@
-new Vue({
-    el: 'sport',
-    data: {
-        item: items[3]
+const javaPackagesApp = {
+    data() {
+        return {
+            mainHeading: 'Discover Canada',
+            packages,
+            isFlipped: false,
+            learnMoreUrl: 'https://docs.oracle.com/javase/tutorial/',
+            userInput: 'Default',
+            index: 0
+        }
+    },
+    methods: {
+        move(change) {
+            if (this.packages[this.index + change]) {
+                this.index += change;
+                this.isFlipped = false;
+            }
+        }
     }
-});
+}
+
+const app = Vue.createApp(javaPackagesApp).mount("#app");
